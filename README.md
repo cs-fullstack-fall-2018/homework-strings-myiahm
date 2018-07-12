93b3f292cbae53f72b2ec12685512091669eaf1d
Myiah Moore <myiahsade@gmail.com>
2018-07-11 20:23:20 -0500

    check in

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Homework Problem 1: Naughty Strings
 
 What is the error message returned when you improperly use quotes inside of strings?
 
+
+
 Provide an example and explain the error message.
 
 Homework Problem 2: Fruits and Vegetables
@@ -12,3 +14,7 @@ Homework Problem 2: Fruits and Vegetables
 x = ["apple", "banana", "carrot"]
 
 Write one line of code that when executed returns "apples bananas and carrots".
+
+
+
+
---
+++ answers1.py
@@ -0,0 +1,13 @@
+#problem1: SyntaxError: EOL while scanning string literal
+
+#input("why no quotes)
+ #error message File "answers1.py", line 1
+#input("why no quotes)
+      #^
+      #SyntaxError: EOL while scanning string literal
+#The EOL error code means pyhton hit the end of a line going through a string because either the quotation marks were
+#forgotten at the end or beginning.
+
+#problem2:
+x = ["apple", "banana", "carrot"]
+print("apples" , "bananas" , "and" , "carrots")
